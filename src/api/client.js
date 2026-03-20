48cac92a7287af1c31f2f060da379fd9c66dbb3c
ykhokage <denlar1989@gmail.com>
2026-03-20 19:02:02 +0400

    delete localhost2

--- src/api/client.js
+++ src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { clearAuth, getToken } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3000",
   withCredentials: false,
 });
 
